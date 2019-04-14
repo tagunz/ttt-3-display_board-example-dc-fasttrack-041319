546b10979693c859de38b7732209d65469348624
Tomas Gunz <tagunz@hotmail.com>
2019-04-14 14:41:13 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,8 +4,11 @@ board = [" "," "," "," "," "," "," "," "," "]
 row_one = (" " + board[0] + " | " + board[1] + " | " + board[2] + " ")
 row_two = " " + board[3] + " | " + board[4] + " | " + board[5] + " "
 row_three = " " + board[6] + " | " + board[7] + " | " + board[8] + " "
-puts " 1 | 2 | 3"
+
+board_s def
+ puts " 1 | 2 | 3"
  puts "-----------"   
  puts " 4 | 5 | 6"
  puts "-----------"
  puts " 7 | 8 | 9"
+ end
