26188ac6467cc515860963943eb22c1be984678d
Tomas Gunz <tagunz@hotmail.com>
2019-04-14 14:39:50 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,11 +4,8 @@ board = [" "," "," "," "," "," "," "," "," "]
 row_one = (" " + board[0] + " | " + board[1] + " | " + board[2] + " ")
 row_two = " " + board[3] + " | " + board[4] + " | " + board[5] + " "
 row_three = " " + board[6] + " | " + board[7] + " | " + board[8] + " "
-if #{"start"} == true
-  then
-    puts row_one
-    puts"-----------"
-    puts row_two  
-    puts "-----------"
-    puts row_three
-end
+puts " 1 | 2 | 3"
+ puts "-----------"   
+ puts " 4 | 5 | 6"
+ puts "-----------"
+ puts " 7 | 8 | 9"
